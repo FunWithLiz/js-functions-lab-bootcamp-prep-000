26a81f92871e64433b0f5ae5bd11c2cd42023732
elizabethfun@gmail.com <elizabethfun@gmail.com>
2018-03-17 04:14:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+
+function happyHolidays() { 
+  if("Happy holidays") return("Happy holidays!")
+}
+
+function happyHolidaysTo(name){
+  if(happyHolidaysTo(name)==="Happy holidays, you!")
+  return "Happy holidays, ${name}!"
+}
